3b7e100bc35b3c00b7a21861388f5e98eaa40160
CrossingScripts <xjustteddyx@gmail.com>
2026-01-07 17:20:51 -0500

    Server update Bug Fix

--- server/binoculars.lua
+++ server/binoculars.lua
@@ -1,33 +1,21 @@
 local RSGCore = exports['rsg-core']:GetCoreObject()
 
-print('[Crossing-Binoculars] Server script loaded')
+print('[Crossing-Binoculars] Server script loaded (Client-authoritative)')
 
+-- Normal Binoculars
 RSGCore.Functions.CreateUseableItem('weapon_kit_binoculars', function(source)
-    print('[Crossing-Binoculars] Binoculars item used by player:', source)
-    local xPlayer = RSGCore.Functions.GetPlayer(source)
-    if xPlayer then
-        TriggerClientEvent('Crossing-Binoculars:client:RemoveBinoculars', source, 'WEAPON_KIT_BINOCULARS')
-        Wait(100) -- Ensure removal is processed before re-equipping
-        TriggerClientEvent('Crossing-Binoculars:client:EquipBinoculars', source, 'WEAPON_KIT_BINOCULARS')
-    end
+    TriggerClientEvent(
+        'Crossing-Binoculars:client:EquipBinoculars',
+        source,
+        'WEAPON_KIT_BINOCULARS'
+    )
 end)
 
+-- Improved Binoculars
 RSGCore.Functions.CreateUseableItem('weapon_kit_binoculars_improved', function(source)
-    print('[Crossing-Binoculars] Improved binoculars item used by player:', source)
-    local xPlayer = RSGCore.Functions.GetPlayer(source)
-    if xPlayer then
-        TriggerClientEvent('Crossing-Binoculars:client:RemoveBinoculars', source, 'WEAPON_KIT_BINOCULARS_IMPROVED')
-        Wait(100) -- Ensure removal is processed before re-equipping
-        TriggerClientEvent('Crossing-Binoculars:client:EquipBinoculars', source, 'WEAPON_KIT_BINOCULARS_IMPROVED')
-    end
-end)
-
-RSGCore.Functions.CreateUseableItem('remove_weapon_kit_binoculars', function(source)
-    print('[Crossing-Binoculars] Removing binoculars from player:', source)
-    TriggerClientEvent('Crossing-Binoculars:client:RemoveBinoculars', source, 'WEAPON_KIT_BINOCULARS')
-end)
-
-RSGCore.Functions.CreateUseableItem('remove_weapon_kit_binoculars_improved', function(source)
-    print('[Crossing-Binoculars] Removing improved binoculars from player:', source)
-    TriggerClientEvent('Crossing-Binoculars:client:RemoveBinoculars', source, 'WEAPON_KIT_BINOCULARS_IMPROVED')
+    TriggerClientEvent(
+        'Crossing-Binoculars:client:EquipBinoculars',
+        source,
+        'WEAPON_KIT_BINOCULARS_IMPROVED'
+    )
 end)
